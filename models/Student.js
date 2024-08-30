9bd1bdd38ffdd54f67a3a67f2523834295a7b8e5
JohnLlenDuenas <152172679+JohnLlenDuenas@users.noreply.github.com>
2024-08-30 08:53:04 +0800

    update

--- models/Student.js
+++ models/Student.js
@@ -1,22 +1,16 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  studentNumber: String,
-  email: String,
-  password: String,
-  iv: String,
-  key: String,
-  consentfilled: {
-    type: Boolean,
-    default: false
-  },
-  passwordChanged: {
-    type: Boolean,
-    default: false
-  },
-  accountType: String
+  studentNumber: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  iv: String, // Initialization vector for AES encryption
+  key: String, // Optional: if you are using a unique key per user
+  consentfilled: { type: Boolean, default: false },
+  passwordChanged: { type: Boolean, default: false },
+  accountType: String,
+  birthday: { type: String, required: true } // Store birthday as YYYYMMDD
 });
-
 
 const Student = mongoose.model('Student', studentSchema);
 module.exports = Student;
--- run.bat
+++
@@ -1,2 +0,0 @@
-start msedge http://localhost:3000/
-node app.js
